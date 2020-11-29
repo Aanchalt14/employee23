5e4f5f5fe8b579c3862a7750fe849e17012a2566
Aanchalt14 <aanchalt71@gmail.com>
2020-11-30 00:41:59 +0530

    Added : part time of employee

---
+++ partTime.java
@@ -0,0 +1,20 @@
+ int IS_PART_TIME=1;
+   int IS_FULL_TIME=2;
+   int EMP_RATE_PER_HR=20;
+   Random rand=new Random();
+   int empAttendance=rand.nextInt(2);
+   System.out.println("Staus of Employee Whether Employee is present or absent:"+empAttendance);
+   if(empAttendance == IS_PART_TIME) {
+        empHrs=4;
+   }
+   else if(empAttendance == IS_FULL_TIME) {
+			empHrs=8;
+   }
+	else
+			empHrs=0;
+
+ 
+  int salary=empHrs * EMP_RATE_PER_HR;
+  System.out.println("Salary: "+salary);
+  }
+}
