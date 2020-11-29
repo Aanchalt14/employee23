098f7803a437664bf4a9d1699880f4a1b057279b
Aanchalt14 <aanchalt71@gmail.com>
2020-11-30 00:56:08 +0530

    Added : monthlywage of employee

---
+++ monthlywage.java
@@ -0,0 +1,31 @@
+ int empHrs;
+   int salary;
+   int IS_PART_TIME=1;
+   int IS_FULL_TIME=2;
+   int EMP_RATE_PER_HR=20;
+   int NUM_WORKING_DAYS=20;
+   int totalSalary=0;
+
+   for(int day=1;day<=NUM_WORKING_DAYS;day++ )
+   {
+    Random rand=new Random();
+    int empAttendance=rand.nextInt(3);
+    switch (empAttendance) {
+    case 1:
+           empHrs=4;
+           salary=(empHrs*EMP_RATE_PER_HR);
+           break;
+    case 2:
+           empHrs=8;
+           salary=(empHrs*EMP_RATE_PER_HR);
+           break;
+    default:
+           empHrs=0;
+           salary=(empHrs*EMP_RATE_PER_HR);
+           break;
+    }
+    totalSalary=(totalSalary+salary);
+   }
+   System.out.println("Total Salary: "+totalSalary); 
+}
+}
