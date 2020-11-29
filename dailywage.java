55e23b157a6bb613def42c3a6ba188e505073e2c
Aanchalt14 <aanchalt71@gmail.com>
2020-11-30 00:26:05 +0530

    Added : daily wage of employee

---
+++ dailywage.java
@@ -0,0 +1,16 @@
+ if(empAttendance == isPresent) {
+
+           int isPresent=1;
+   int salary;
+   Random rand=new Random();
+System.out.println("Employee is present...");
+        int empHrs=8;
+        int ratePerHour=20;
+        salary=empHrs * ratePerHour;
+   }
+   else {
+
+        System.out.println("Employee is absent...");
+        salary=0;
+   }
+  System.out.println("Salary of Employee: "+salary);
